b5d2b62545c0baeeac0ec661f31538831c2070aa
Alonso-AS <alfredoalonso_alcaraz@ucol.mx>
2021-05-24 16:42:48 -0500

    Modification of the registration and login screens
    New attributes have been added to the Tinput component, and also a new "Tinput" component on the registration screen, for password verification.

--- DenunciaCiudadana/src/components/Tinput.js
+++ DenunciaCiudadana/src/components/Tinput.js
@@ -2,10 +2,16 @@ import React from 'react';
 import {TextInput, View} from 'react-native';
 import styles from '../assets/styles/styles';
 
-const Tinput = () => {
+const Tinput = ({labelValue, placeholderText, ...rest}) => {
   return (
     <View>
-      <TextInput style={styles.rect} />
+      <TextInput style={styles.rect}
+        value={labelValue}
+        numberOfLines={1}
+        placeholder={placeholderText}
+        placeholderTextColor="#666"
+      {...rest}
+      />
     </View>
   );
 };
